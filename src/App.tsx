5770050c91236251f61da9e9737824f29d0a6c0a
beasou <beatrizreginasouza1@gmail.com>
2023-09-05 12:59:38 -0300

    Alterando estilização  global

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header";
+import { Task } from "./components/Task";
 
 import "./global.css";
 
@@ -7,6 +8,10 @@ function App() {
   return (
     <>
       <Header/>
+      <Task/>
+      <Task/>
+      <Task/>
+      <Task/>
     </>
   )
 }
